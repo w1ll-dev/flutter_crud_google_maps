cf90ac04ffaf6cbbf31471479ef6c8e84ba67da2
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-09 18:03:39 -0300

    add initial verify token method to AuthController

--- backend/src/controllers/AuthController.js
+++ backend/src/controllers/AuthController.js
@@ -29,4 +29,22 @@ module.exports = {
             token: null
         });
     },
+
+    verifyToken(req, res, next) {
+        const token = req.headers["x-access-token"];
+
+        if (!token) return res.status(401).json({
+            auth: false,
+            message: "no token provided"
+        });
+
+        jwt.verify(token, secret, (err, decoded) => {
+            if(err) return res.status(500).json({
+                auth: false,
+                message: "Failed to authenticate token"
+            });
+            req.userId = decoded.id;
+            next();
+        })
+    }
 }
