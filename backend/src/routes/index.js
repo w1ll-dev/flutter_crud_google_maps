d27ecc66b708df089cde9c2b0562da76719a54d4
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-09 20:01:08 -0300

    fix file name

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const AuthController = require('../controllers/AuthController');
+const EstablishmentsController = require('../controllers/EstablishmentsController');
 
 router
     .post('/login', AuthController.login)
     .get('/logout', AuthController.logout)
-
+    
+    .get('/establishments', EstablishmentsController.index)
 module.exports = router;
