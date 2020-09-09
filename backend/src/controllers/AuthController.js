22f8c6eb0405518090f0027157cee04ea6e06fd5
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-09 18:02:43 -0300

    add initial login method to AuthController

---
+++ backend/src/controllers/AuthController.js
@@ -0,0 +1,26 @@
+const knex = require('../database/');
+const encrypt = require('../utils/encrypt');
+const jwt = require('jsonwebtoken');
+const secret = "Q3Tg9*cGN!m`EF~V"
+
+module.exports = {
+    async login(req, res) {
+        const { name, password } = req.body;
+        const passwordEncrypted = encrypt(password);
+        const listUsers = await knex('users');
+
+        for (let user of listUsers) {
+            if (name === user.name && passwordEncrypted === user.password) {
+                const id = 1;
+                const token = jwt.sign({ id }, secret, { expiresIn: '1h' })
+                return res.status(200).json({
+                    auth: true,
+                    token
+                });
+            } else {
+                return res.status(403).send("invalid user or password");
+            }
+        }
+    },
+
+}
