e5fb0de6b03744a102c38fcb47670c373fc82dbf
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-09 17:55:04 -0300

    add knexfile with config

---
+++ backend/knexfile.js
@@ -0,0 +1,19 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      database: "sdx_tech_code",
+      user: "postgres",
+      password: "root",
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    }
+  },
+};
