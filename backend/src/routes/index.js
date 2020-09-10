5adf7649e694ec583cc64bc56a2a76d54d79fde9
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-10 17:48:33 -0300

    temporary change to test with no login

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -8,7 +8,8 @@ router
     .post('/login', AuthController.login)
     .get('/logout', AuthController.logout)
 
-    .get('/establishments', AuthController.verifyToken, EstablishmentsController.index)
+    // .get('/establishments', AuthController.verifyToken, EstablishmentsController.index)
+    .get('/establishments', EstablishmentsController.index)
     .post('/establishments', AuthController.verifyToken, EstablishmentsController.create)
     .put('/establishments/:id', AuthController.verifyToken, EstablishmentsController.update)
     .delete('/establishments/:id', AuthController.verifyToken, EstablishmentsController.delete)
