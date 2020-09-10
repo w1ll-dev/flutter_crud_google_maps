dfab32a7a9c5244fe3b6e9e8fb767cf97a80124d
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-10 15:40:55 -0300

    change initial login

--- front_mobile/lib/src/views/pages/login_page.dart
+++ front_mobile/lib/src/views/pages/login_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('login'),
+    return Scaffold(
+      body: Center(
+        child: Text('login'),
+      ),
     );
   }
 }
