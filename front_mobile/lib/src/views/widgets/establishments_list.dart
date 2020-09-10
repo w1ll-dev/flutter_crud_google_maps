709eeae15c36dd5cdc6e9266e1f6f9d9ef448b1f
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-10 17:53:04 -0300

    add establishments list widget

---
+++ front_mobile/lib/src/views/widgets/establishments_list.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:front_mobile/src/controllers/establishments_controller.dart';
+import 'package:front_mobile/src/views/widgets/establishments_tile.dart';
+import 'package:get_it/get_it.dart';
+
+class EstablishmentsList extends StatelessWidget {
+  final establishmentsController = GetIt.I.get<EstablishmentsController>();
+  @override
+  Widget build(BuildContext context) {
+    establishmentsController.getList();
+    return Observer(
+      builder: (_) {
+        return ListView.builder(
+          itemCount: 1,
+          itemBuilder: (context, index) {
+            return EstablishmentsTile(
+              establishment: establishmentsController.all[index],
+            );
+          },
+        );
+      },
+    );
+  }
+}
