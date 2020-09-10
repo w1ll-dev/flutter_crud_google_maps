d60d0a4cd05346043915dc5632c95c660d9266ff
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-10 17:50:44 -0300

    add establishment repository with get establishments method

---
+++ front_mobile/lib/src/repositories/establishments_repository.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:front_mobile/src/models/establishment_model.dart';
+import "package:http/http.dart" as http;
+
+const _serverIp = '10.0.0.118';
+const _serverPort = '5000';
+const _baseUrl = "http://$_serverIp:$_serverPort";
+
+Future<List<Establishment>> getEstablishments() async {
+  final response = await http.get("$_baseUrl/establishments");
+  List<dynamic> jsonRes = jsonDecode(response.body);
+  List<Establishment> list = [];
+
+  jsonRes.forEach((establishment) {
+    list.add(Establishment.fromJson(establishment, establishment['id']));
+  });
+
+  return response.statusCode == 200 ? list : Exception("Fail");
+}
