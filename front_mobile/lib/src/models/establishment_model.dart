eb97d1a79c3155024da3fc580e680d11eb0265e8
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-10 17:50:05 -0300

    add establishment model

---
+++ front_mobile/lib/src/models/establishment_model.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+
+class Establishment {
+  final int id;
+  final String name;
+  final double lat;
+  final double lng;
+  final String createdAt;
+  final String updatedAt;
+
+  Establishment({
+    this.id,
+    @required this.name,
+    @required this.lat,
+    @required this.lng,
+    this.createdAt,
+    this.updatedAt,
+  });
+
+  factory Establishment.fromJson(Map<String, dynamic> json, int id) {
+    return Establishment(
+      id: json["id"],
+      name: json["name"],
+      lat: json["lat"],
+      lng: json["lng"],
+      createdAt: json["created_at"],
+      updatedAt: json["updated_at"],
+    );
+  }
+}
