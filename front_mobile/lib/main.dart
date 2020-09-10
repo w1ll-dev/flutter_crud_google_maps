4e387bf352e83d32b3100bf68c359458faf08e33
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-10 17:49:42 -0300

    add global state with get_it

--- front_mobile/lib/main.dart
+++ front_mobile/lib/main.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:front_mobile/src/views/pages/login_page.dart';
+import 'package:front_mobile/src/controllers/establishments_controller.dart';
+// import 'package:front_mobile/src/views/pages/login_page.dart';
+import 'package:get_it/get_it.dart';
 
-void main() => runApp(App());
+import 'src/views/pages/dashboard.dart';
+
+void main() {
+  GetIt getIt = GetIt.I;
+  getIt.registerSingleton<EstablishmentsController>(EstablishmentsController());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
+}
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Front Mobile',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: LoginPage(),
+      home: Dashboard(),
     );
   }
 }
