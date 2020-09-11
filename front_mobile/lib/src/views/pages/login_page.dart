19f3492751056c8e6793bb7d535061529ac00cfa
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-11 14:19:08 -0300

    add initial login page

--- front_mobile/lib/src/views/pages/login_page.dart
+++ front_mobile/lib/src/views/pages/login_page.dart
@@ -1,17 +1,52 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:front_mobile/src/controllers/establishments_controller.dart';
+import '../widgets/name_input.dart';
+import '../widgets/password_input.dart';
+import '../widgets/enter_login_buttom.dart';
 
 class LoginPage extends StatelessWidget {
-  final EstablishmentsController establishmentsController =
-      EstablishmentsController();
   @override
   Widget build(BuildContext context) {
-    establishmentsController.getList();
+    Size screen = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Text(
-          'login',
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Flex(
+            direction: Axis.vertical,
+            children: <Widget>[
+              Flexible(
+                flex: 5,
+                child: Center(
+                  child: Image.asset(
+                    "assets/images/logo.png",
+                    width: screen.width * 0.8,
+                    height: screen.width * 0.8,
+                  ),
+                ),
+              ),
+              Flexible(
+                flex: 3,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    NameInput(),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    PasswordInput(),
+                  ],
+                ),
+              ),
+              Flexible(
+                flex: 3,
+                child: Center(
+                  child: AnimatedButton(),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
