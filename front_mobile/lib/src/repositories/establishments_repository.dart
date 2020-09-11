876fa0bb17706874ca8e6e8ab9c1c8bff724b0ad
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-11 17:20:36 -0300

    add getTokenFromSF method to use auth token on requests

--- front_mobile/lib/src/repositories/establishments_repository.dart
+++ front_mobile/lib/src/repositories/establishments_repository.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import '../models/establishment_model.dart';
 import "package:http/http.dart" as http;
 import './server_config.dart';
@@ -9,22 +11,34 @@ final _serverIp = server.serverIp;
 final _serverPort = server.serverPort;
 final _baseUrl = "http://$_serverIp:$_serverPort";
 
-const _userToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTk5NzkxNDExLCJleHAiOjE1OTk3OTUwMTF9.14yf7RD4tVrS7DRyg6btFE-S_Eezap4Ok7AHl3EOSK8";
+getTokenFromSF() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String stringValue = prefs.getString('token');
+  return stringValue;
+}
 
 Future<List<Establishment>> getEstablishments() async {
-  final response = await http.get("$_baseUrl/establishments");
+  final _userToken = await getTokenFromSF();
+  final response = await http.get(
+    "$_baseUrl/establishments",
+    headers: {
+      "x-access-token": "$_userToken",
+      "Content-Type": "application/json"
+    },
+  );
+  print(response.body);
   List<dynamic> jsonRes = jsonDecode(response.body);
   List<Establishment> list = [];
 
   jsonRes.forEach((establishment) {
-    list.add(Establishment.fromJson(establishment, establishment['id']));
+    list.add(Establishment.fromJson(establishment));
   });
 
   return response.statusCode == 200 ? list : Exception("Fail");
 }
 
 Future<void> createEstablishment({Establishment establishment}) async {
+  final _userToken = await getTokenFromSF();
   http.post("$_baseUrl/establishments",
       body: jsonEncode({
         'name': establishment.name,
@@ -38,6 +52,7 @@ Future<void> createEstablishment({Establishment establishment}) async {
 }
 
 Future<void> updateEstablishment({Establishment establishment}) async {
+  final _userToken = await getTokenFromSF();
   http.put("$_baseUrl/establishments/${establishment.id}",
       body: jsonEncode({
         'name': establishment.name,
@@ -51,6 +66,7 @@ Future<void> updateEstablishment({Establishment establishment}) async {
 }
 
 Future<void> deleteEstablishment({int id}) async {
+  final _userToken = await getTokenFromSF();
   http.delete("$_baseUrl/establishments/$id", headers: {
     "x-access-token": "$_userToken",
     "Content-Type": "application/json"
