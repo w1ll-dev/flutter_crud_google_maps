bc0125bcb0f568bba7d5d75f589436ca2f9229b3
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-10 22:59:58 -0300

    add establishments list tile component

--- front_mobile/lib/src/views/widgets/establishments_tile.dart
+++ front_mobile/lib/src/views/widgets/establishments_tile.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import '../../models/establishment_model.dart';
+import '../utils/go_to_form.dart';
+import '../../controllers/establishment_form_controller.dart';
 
 class EstablishmentsTile extends StatelessWidget {
   final Establishment establishment;
-
+  final _establishmentsController = GetIt.I.get<EstablishmentFormController>();
   EstablishmentsTile({
     @required this.establishment,
   });
@@ -13,7 +16,33 @@ class EstablishmentsTile extends StatelessWidget {
     return ListTile(
       leading: Icon(Icons.map),
       title: Text("${establishment.name}"),
-      subtitle: Text("lat: ${establishment.lat} | lng: ${establishment.lng}"),
+      subtitle: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "lat:",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text("${establishment.lat}"),
+            ],
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "lng:",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text("${establishment.lng}")
+            ],
+          )
+        ],
+      ),
       trailing: Container(
         width: 100,
         child: Row(
@@ -23,21 +52,20 @@ class EstablishmentsTile extends StatelessWidget {
                 Icons.edit,
                 color: Colors.yellow,
               ),
-              onPressed: () => {},
+              onPressed: () => {
+                _establishmentsController.updateStablishment(
+                  newEstablishment: establishment,
+                ),
+                gotToEstablishmentForm(context: context),
+              },
             ),
             IconButton(
-                icon: Icon(
-                  Icons.delete,
-                  color: Colors.red,
-                ),
-                // onPressed: () => showDialog(
-                //   context: context,
-                //   builder: (context) => AlertDelete(
-                //     user: user,
-                //     usersController: usersController,
-                //   ),
-                // ),
-                onPressed: () => {})
+              icon: Icon(
+                Icons.delete,
+                color: Colors.red,
+              ),
+              onPressed: () => {},
+            )
           ],
         ),
       ),
