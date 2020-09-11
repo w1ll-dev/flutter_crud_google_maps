2d78f15d57ac38f2c55d78c8e08a5e0cc9f56d6e
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-11 17:18:55 -0300

    add token token to shared preferences

--- front_mobile/lib/src/controllers/login_controller.dart
+++ front_mobile/lib/src/controllers/login_controller.dart
@@ -1,20 +1,33 @@
 import 'package:front_mobile/src/repositories/login_repository.dart';
 import 'package:mobx/mobx.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 part 'login_controller.g.dart';
 
 class LoginController = _LoginControllerBase with _$LoginController;
 
 abstract class _LoginControllerBase with Store {
   @observable
-  bool validUser = false;
+  bool validUser;
   @observable
   String name = '';
   @observable
   String password = '';
 
+  addStringToSF(String authToken) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('token', authToken);
+  }
+
   @action
   tryLogin() async {
-    await login(name, password);
+    Map<String, dynamic> response = await login(name, password);
+    if (response['auth']) {
+      await addStringToSF(response['token']);
+      validUser = true;
+      print({'validUser': validUser, 'token': response['token']});
+    } else {
+      validUser = false;
+    }
   }
 
   @action
