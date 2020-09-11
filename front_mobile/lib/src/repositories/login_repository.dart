cf19ce57cbc977988099038d7a34fb20884e9121
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-11 20:03:57 -0300

    add try catch to login repository

--- front_mobile/lib/src/repositories/login_repository.dart
+++ front_mobile/lib/src/repositories/login_repository.dart
@@ -10,18 +10,22 @@ final _serverPort = server.serverPort;
 final _baseUrl = "http://$_serverIp:$_serverPort";
 
 Future<Map<String, dynamic>> login(String name, String password) async {
-  final response = await http.post(
-    "$_baseUrl/login",
-    body: jsonEncode({
-      'name': '$name',
-      'password': '$password',
-    }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  );
+  try {
+    final response = await http.post(
+      "$_baseUrl/login",
+      body: jsonEncode({
+        'name': '$name',
+        'password': '$password',
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    );
 
-  Map<String, dynamic> jsonRes = jsonDecode(response.body);
+    Map<String, dynamic> jsonRes = jsonDecode(response.body);
 
-  return jsonRes;
+    return jsonRes;
+  } catch (e) {
+    return {'auth': false};
+  }
 }
