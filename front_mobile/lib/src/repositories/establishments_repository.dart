f89f47ff20e7f79222194618611d31223c05f2f5
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-10 23:04:02 -0300

    add method create to establishments repository

--- front_mobile/lib/src/repositories/establishments_repository.dart
+++ front_mobile/lib/src/repositories/establishments_repository.dart
@@ -6,6 +6,9 @@ const _serverIp = '10.0.0.118';
 const _serverPort = '5000';
 const _baseUrl = "http://$_serverIp:$_serverPort";
 
+const _userToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTk5Nzg3NDYwLCJleHAiOjE1OTk3OTEwNjB9.VbNtiF-Jcqm1TaA8lnWSFzhQ9j03lFWgn2dCYxh21wg";
+
 Future<List<Establishment>> getEstablishments() async {
   final response = await http.get("$_baseUrl/establishments");
   List<dynamic> jsonRes = jsonDecode(response.body);
@@ -19,12 +22,14 @@ Future<List<Establishment>> getEstablishments() async {
 }
 
 Future<void> createEstablishment({Establishment establishment}) async {
-  http
-      .post("$_baseUrl/establishment",
-          body: jsonEncode({
-            'name': establishment.name,
-            'lat': establishment.lat,
-            'lng': establishment.lng,
-          }))
-      .then((value) => value.statusCode);
+  http.post("$_baseUrl/establishments",
+      body: jsonEncode({
+        'name': establishment.name,
+        'lat': establishment.lat,
+        'lng': establishment.lng,
+      }),
+      headers: {
+        "x-access-token": "$_userToken",
+        "Content-Type": "application/json"
+      }).then((value) => value.statusCode);
 }
