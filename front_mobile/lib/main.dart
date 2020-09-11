7cdeb3c7a68f4ee05a61566350c52ece72186f51
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-10 23:02:48 -0300

    add controllers to global state

--- front_mobile/lib/main.dart
+++ front_mobile/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:front_mobile/src/controllers/establishment_form_controller.dart';
 import 'package:front_mobile/src/controllers/establishments_controller.dart';
-// import 'package:front_mobile/src/views/pages/login_page.dart';
 import 'package:get_it/get_it.dart';
 
 import 'src/views/pages/dashboard.dart';
@@ -8,6 +8,8 @@ import 'src/views/pages/dashboard.dart';
 void main() {
   GetIt getIt = GetIt.I;
   getIt.registerSingleton<EstablishmentsController>(EstablishmentsController());
+  getIt.registerSingleton<EstablishmentFormController>(
+      EstablishmentFormController());
   WidgetsFlutterBinding.ensureInitialized();
   runApp(App());
 }
