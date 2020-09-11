8ffd2423747436a3611031b293342fb19b4eb589
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-11 14:18:44 -0300

    change main file to redirect user to login page

--- front_mobile/lib/main.dart
+++ front_mobile/lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:front_mobile/src/controllers/establishment_form_controller.dart';
 import 'package:front_mobile/src/controllers/establishments_controller.dart';
+import 'package:front_mobile/src/views/pages/login_page.dart';
 import 'package:get_it/get_it.dart';
-
-import 'src/views/pages/dashboard.dart';
 
 void main() {
   GetIt getIt = GetIt.I;
@@ -24,7 +23,7 @@ class App extends StatelessWidget {
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Dashboard(),
+      home: LoginPage(),
     );
   }
 }
