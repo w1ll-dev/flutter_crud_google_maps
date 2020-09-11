8ff4b1447dc0a4b972e2a46990d8c7257f19de92
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-10 23:00:35 -0300

    add dashboard page

--- front_mobile/lib/src/views/pages/dashboard.dart
+++ front_mobile/lib/src/views/pages/dashboard.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:front_mobile/src/views/utils/go_to_form.dart';
 import 'package:front_mobile/src/views/widgets/establishments_list.dart';
+import 'package:get_it/get_it.dart';
+import '../../controllers/establishment_form_controller.dart';
 
 class Dashboard extends StatelessWidget {
+  final _establishmentsFormController =
+      GetIt.I.get<EstablishmentFormController>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,6 +14,13 @@ class Dashboard extends StatelessWidget {
         title: Text('Establishments'),
       ),
       body: EstablishmentsList(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => {
+          _establishmentsFormController.createNewEstablishment(),
+          gotToEstablishmentForm(context: context)
+        },
+      ),
     );
   }
 }
