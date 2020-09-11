34954752af9cf2752c9a6ea68ebb14680725c0c7
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-11 16:29:26 -0300

    add singleton to login controller

--- front_mobile/lib/main.dart
+++ front_mobile/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:front_mobile/src/controllers/establishment_form_controller.dart';
 import 'package:front_mobile/src/controllers/establishments_controller.dart';
+import 'package:front_mobile/src/controllers/login_controller.dart';
 import 'package:front_mobile/src/views/pages/login_page.dart';
 import 'package:get_it/get_it.dart';
 
 void main() {
   GetIt getIt = GetIt.I;
   getIt.registerSingleton<EstablishmentsController>(EstablishmentsController());
+  getIt.registerSingleton<LoginController>(LoginController());
   getIt.registerSingleton<EstablishmentFormController>(
       EstablishmentFormController());
   WidgetsFlutterBinding.ensureInitialized();
